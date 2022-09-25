7518f160b5f4a905cd3f2d5705e009213dac8059
Jose Antonio Ponqueli Conto <zehconto@gmail.com>
2022-09-25 11:09:09 -0300

    feat: mock post to start css

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,11 @@
 import { GetStaticProps } from 'next';
+import Head from 'next/head';
+import Link from 'next/link';
 
 import { getPrismicClient } from '../services/prismic';
 
 import commonStyles from '../styles/common.module.scss';
+import { FiCalendar, FiUser } from 'react-icons/fi';
 import styles from './home.module.scss';
 
 interface Post {
@@ -12,7 +15,7 @@ interface Post {
     title: string;
     subtitle: string;
     author: string;
-  };
+  }
 }
 
 interface PostPagination {
@@ -24,9 +27,35 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-// export default function Home() {
-//   // TODO
-// }
+export default function Home() { //{ postsPagination: { next_page, results } }: HomeProps
+  return (
+    <>
+      <Head>
+        <title>Posts | SpaceTraveling</title>
+      </Head>
+      <main className={styles.container}>
+        <article className={styles.post}>
+          <Link href={`/post/test`}>
+            <a>
+              <h1>Titulo</h1>
+              <p>Subtitulo</p>
+              <div className={styles.postCreation}>
+                <time>
+                  <FiCalendar />
+                  15 Mar 2021
+                </time>
+                <p>
+                  <FiUser />
+                  Autor
+                </p>
+              </div>
+            </a>
+          </Link>
+        </article>
+      </main>
+    </>
+  )
+}
 
 // export const getStaticProps = async () => {
 //   // const prismic = getPrismicClient({});
